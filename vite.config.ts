ce6f16c2de413514fc6beeb2fb98b52d101096ca
Durgrim (Esetban) <goldmuros@yahoo.com>
2025-09-22 20:15:10 +0200

    yaml deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/pase-uti/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
